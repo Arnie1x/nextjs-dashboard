89800afd79e7819fd608ee1ba9e3af3b0675da35
Arnie <arnie752@gmail.com>
2025-02-05 11:33:28 +0300

    feat(app/dashboard/invoices/[id]/edit/): Created the  Edit and Delete Invoice Functionality
    A user can now delete and modify invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,7 +5,7 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
  
-const CreateInvoiceFormSchema = z.object({
+const InvoiceFormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
@@ -13,7 +13,7 @@ const CreateInvoiceFormSchema = z.object({
   date: z.string(),
 });
  
-const CreateInvoice = CreateInvoiceFormSchema.omit({ id: true, date: true });
+const CreateInvoice = InvoiceFormSchema.omit({ id: true, date: true });
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
@@ -31,4 +31,31 @@ export async function createInvoice(formData: FormData) {
   `;
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = InvoiceFormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
